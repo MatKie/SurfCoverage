97d83b8d81e966f9ee3dd60214b2f7f6cc0e4d63
MatKie <mtth.kiesel@gmail.com>
2021-04-08 16:25:11 +0100

    changed template structure

---
+++ run_VLE.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+temp=298.15
+surfactants=$1
+tot_surfactants=$(($1+$1))
+name='SDS'
+c_ion='NA'
+dir='VLE_'${surfactants}
+
+if [ -d $dir ];
+then
+    rm -r $dir
+fi
+
+cp -r template_VLE $dir
+mkdir $dir/topo
+cp  topo/topol.top $dir/topo
+cd $dir
+echo '*' > .gitignore
+
+sed -i s/NRSURFACTANT/$surfactants/g topo/topol.top
+echo ${name}' '${tot_surfactants} >> topo/topol.top
+
+cd config 
+sed -i s/NRSURFACTANT/$surfactants/g water_SDS.inp
+/home/mk8118/packmol/packmol < water_SDS.inp
+
+gmx editconf -f water_SDS.pdb -box 8 8 40 -o water_SDS.gro
+gmx grompp -f dummy.mdp -c water_SDS.gro -p ../topo/topol.top 
+gmx genion -s topol.tpr -p ../topo/topol.top -o water_SDS_neutral.gro -neutral -pname $c_ion << EOF
+2
+EOF
+
+gmx grompp -f dummy.mdp -c water_SDS_neutral.gro -p ../topo/topol.top 
+
+gmx make_ndx -f topol.tpr << EOF
+t W2
+t SO4V9
+t CM
+t CT
+t NA+
+q
+EOF
+
+cd ../eq_nvt
+
+gmx grompp -f nvt.mdp -c ../config/water_SDS_neutral.gro -p ../topo/topol.top -n ../config/index.ndx
+mv GROMACS.sh SC_VLE_${surfactants}.sh
+ 
---
+++ run_VLEs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./run_VLE.sh 1
+./run_VLE.sh 10
+./run_VLE.sh 20
+./run_VLE.sh 30
+./run_VLE.sh 40
+./run_VLE.sh 50
+./run_VLE.sh 60
+./run_VLE.sh 70
+
