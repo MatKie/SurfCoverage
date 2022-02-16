45f3d6ddfa7057423e0b26336dfff54fc4d30bc4
MatKie <mtth.kiesel@gmail.com>
2022-02-16 15:28:04 +0000

    Add twostate isotherm and Bui et al results

--- analysis/surface_tension_lle.py
+++ analysis/surface_tension_lle.py
@@ -62,12 +62,45 @@ def get_data(prop, interaction_correction=True, in_kelvin=False):
     return coverage, prop_averages, prop_errors
 
 
+# +
+from scipy.interpolate import interp1d
+coverage = np.loadtxt('twostate_cov.csv', delimiter=',')
+surftens = np.loadtxt('twostate_tens.csv', delimiter=',')
+gamma = np.loadtxt('gamma_tot.csv', delimiter=',')
 
+f_cov = interp1d(coverage[:, 0], coverage[:, 1], kind='cubic')
+f_surf = interp1d(surftens[:, 0], surftens[:,1], kind='cubic')
+f_gamma = interp1d(gamma[:, 0], gamma[:,1], kind='cubic')
+f_gamma_t = lambda x: 6.022e5*f_gamma(x)
+
+c_min = max([surftens[0,0], gamma[0,0]])
+c_max = min([surftens[-1,0], gamma[-1,0]])
+concentrations = np.geomspace(c_min, c_max)
+
+# +
+bui_simulation = np.loadtxt('bui_simulation.csv', delimiter=',')
+bui_experimental = np.loadtxt('bui_experimental.csv', delimiter=',')
+coverage = np.loadtxt('Frumkin_cov.csv', delimiter=',') # Coverage Frumkin isoT, Llamas2018
+surftens = np.loadtxt('Frumkin_tens.csv', delimiter=',') # Surftens Frumkin isoT, Llamas2018
+
+frumkin_cov = interp1d(coverage[:, 0], coverage[:, 1], kind='cubic')
+frumkin_surf = interp1d(surftens[:, 0], surftens[:, 1], kind='cubic')
+frumkin_gamma_t = lambda x, omega: 6.022e5*frumkin_cov(x)/omega
+omega_llamas = 5.5e5
+omega_bui =  1.8e5
+omega_llamas = 3.02e5
+
+c_min = max([min(surftens[:,0]), min(coverage[:,0])]) 
+c_max = min([max(surftens[:,0]), max(coverage[:,0])])
+frumkin_conc = np.geomspace(c_min, c_max)
 
 
 
 # +
 p0 = [ 5.50000000e+05,  9.53126622e+05, -3.40000000e+00,  9.00000000e-03]
+#p0 = [ 302521,  0.532, -3.400000000e+00,  9e-3]
+#p0 = [1.80000000e+05,  1.23459503e+05, -1.00000000e+01,  1.04149458e-02]
+
 llamas = [5.5e5, 0.532, -3.4, 0.009]
 
 fig, ax = create_fig(1,1)
@@ -89,15 +122,22 @@ for p in [p0, llamas]:
 
     coverage, surftens, surftens_err = get_data('#Surf*SurfTen', interaction_correction=False)
 
-    ax.plot(gamma, Isotherm.tension(gamma), lw=2, ls='--', label='Frumkin Isotherm')  
+    ax.plot(gamma, Isotherm.tension(gamma), lw=2, ls='--', label='Frumkin Isotherm', color='C0')  
+    ax.plot(f_gamma_t(concentrations), f_surf(concentrations), lw=2, ls='--', label='Two State Isotherm', color='C1')
+    #ax.plot(bui_experimental[:, 0]*6.022e5, bui_experimental[:, 1], lw=2, ls='--', label='Bui Experimental')
+    ax.plot(bui_simulation[:, 0]*6.022e5, bui_simulation[:, 1], lw=2, ls='', marker='d', label='Atomistic Simulation', color='C3')
+    #ax.plot(frumkin_gamma_t(frumkin_conc, omega_llamas), frumkin_surf(frumkin_conc), lw=2, label='omega_llamas')
+    ax.plot(frumkin_gamma_t(frumkin_conc, omega_bui), frumkin_surf(frumkin_conc), label='omega_bui', ls='--', color='C3', lw=2)
+
     break
 ax.errorbar(coverage, [prop_average/20. for prop_average in surftens], 
             [prop_error/20. for prop_error in surftens_err], marker='o', color='k', label='This Work', ls='')
 #ax.plot(poly_cov(concentrations), poly_tens(concentrations), lw=2, ls='--', label='Two-State Model')
 
 ax.set_xlabel('$\Gamma\,/\,nm^{-2}$')
-ax.set_ylabel('Surface Tension$\,/\,mN\,m^{-1}$')
-ax.legend()
+ax.set_ylabel('Interfacial Tension$\,/\,mN\,m^{-1}$')
+#ax.legend()
+ax.set_xlim(0, 4.5)
 save_to_file(os.path.join('LLE', 'Surface_tension'))
 
 
